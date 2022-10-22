193ad2bda13a4fcbe18e8d39338aab30e332cee7
Frawley <frawley@duck.com>
2022-10-22 12:34:45 -0400

    Remove endpoint for hashing files

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,9 @@
-import { Router } from "itty-router";
-
-interface Env {
-  HASHER_WORKER: ServiceWorkerGlobalScope;
-}
-
-const Version = {
-  Internal: 1,
-  Public: 0,
-};
-
 export default {
-  async fetch(request: Request, env: Env): Promise<Response> {
-    const router = Router();
+  async fetch(request: Request): Promise<Response> {
+    const requestUrl = new URL(request.url);
 
-    router.post(
-      `/internal/v${Version.Internal}/hashes`,
-      async (req: Request) => {
-        return await env.HASHER_WORKER.fetch(req);
-      }
-    );
-
-    return await router.handle(request);
+    return new Response(`Endpoint ${requestUrl.pathname} not found.`, {
+      status: 404,
+    });
   },
 };
